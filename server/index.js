93757c0bf1ab6eb153be6f3741e36515d16ac75e
Ahmed Al-Sabounchi <a_m_96@live.com>
2022-08-22 12:33:27 -0400

    First commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import 'dotenv/config'
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import 'path'
+
+import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
+
+const app = express()
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+app.use('/posts', postRoutes)
+app.use('/user', userRoutes)
+const PORT = process.env.PORT || 5000
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('./client/build'))
+}
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error))
